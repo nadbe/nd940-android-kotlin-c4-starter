fbd4cad9e9e92f78d4653b14c4b637ba4d2ff0b4
nadbe <d.m.h.becker@gmx.de>
2022-07-30 14:46:37 +0200

    Move data.local tests from androidTest to test package and add tests for RemindersDao

--- starter/app/src/androidTest/java/com/udacity/project4/util/DataBindingIdlingResource.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/util/DataBindingIdlingResource.kt
@@ -21,6 +21,7 @@ import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.testing.FragmentScenario
+
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.IdlingResource
 import java.util.UUID
@@ -104,7 +105,7 @@ fun DataBindingIdlingResource.monitorActivity(
 /**
  * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
  */
-fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
+fun <T : Fragment> DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<T>) {
     fragmentScenario.onFragment {
         this.activity = it.requireActivity()
     }
